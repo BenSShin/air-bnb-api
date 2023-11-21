bb08b3e68291c3258a156da30c78ad96703ba68d
bshin27 <125755440+bshin27@users.noreply.github.com>
2023-11-21 13:26:11 -0600

    added update action

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -26,4 +26,27 @@ class ReservationsController < ApplicationController
       render json: { errors: @reservation.errors.full_messages }, status: :unprocessable_entity
     end
   end
+
+  def update
+    @reservation = Reservation.find_by(id: params[:id])
+    if params[:start_date]
+      start = Date.parse(params[:start_date], "%Y/%m/%d")
+    end
+    if params[:end_date]
+      end_date = Date.parse(params[:end_date], "%Y/%m/%d")
+    end
+    @reservation.update(
+      user_id: params[:user_id] || @reservation.user_id,
+      room_id: params[:room_id] || @reservation.room_id,
+      start_date: start || @reservation.start_date,
+      end_date: end_date || @reservation.end_date,
+      price: params[:price] || @reservation.price,
+      total: params[:total] || @reservation.total,
+    )
+    if @reservation.valid?
+      render :show
+    else
+      render json: { errors: @reservation.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
 end
