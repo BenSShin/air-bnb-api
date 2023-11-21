91830326533678b3ffecd9cd6c378f0180842456
bshin27 <125755440+bshin27@users.noreply.github.com>
2023-11-21 11:21:10 -0600

    Added all routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
-
-  # Hey check out this cool comment.
   get "/addresses" => "addresses#index"
   get "/rooms" => "rooms#index"
   get "/houses" => "houses#index"
+
+  # reservation routes
+  get "reservations" => "reservations#index"
+  get "reservations" => "reservations#show"
+  post "reservations" => "reservations#create"
+  patch "reservations" => "reservations#update"
+  delete "reservations" => "reservations#destroy"
 end
