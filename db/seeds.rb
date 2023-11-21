7b44918f69149d25701092f3f8514274bd10a7c1
bshin27 <125755440+bshin27@users.noreply.github.com>
2023-11-21 10:59:37 -0600

    seeded 2 examples

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+# reservations seeds
+# reservation = Reservation.create(
+#   user_id: 1,
+#   room_id: 1,
+#   start_date: Date.new(2024, 1, 20),
+#   end_date: Date.new(2024, 1, 28),
+#   price: 200,
+#   total: 1600,
+# )
+# reservation = Reservation.create(
+#   user_id: 1,
+#   room_id: 1,
+#   start_date: Date.new(2024, 2, 20),
+#   end_date: Date.new(2024, 2, 28),
+#   price: 200,
+#   total: 1600,
+# )
