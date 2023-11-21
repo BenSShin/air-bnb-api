3c7087bdf94c16898b0679b4ee6f250194092acb
Morgan Winkler <winkler.morgan1553@gmail.com>
2023-11-21 14:59:37 -0600

    Add reviews routes, action, and tests

--- test/controllers/reviews_controller_test.rb
+++ test/controllers/reviews_controller_test.rb
@@ -1,7 +1,45 @@
 require "test_helper"
 
 class ReviewsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "index" do
+    get "/reviews.json"
+    assert_response 200
+
+    data = JSON.parse(response.body)
+    assert_equal Review.count, data.length
+  end
+  
+  test "create" do
+    assert_difference "Review.count", 1 do
+      post "/reviews.json", params: { reservation_id: 1 , rating: 800, comment: "comment" }
+      assert_response 201
+    end
+  end
+  
+  test "show" do
+    get "/reviews/#{Review.first.id}.json"
+    assert_response 200
+
+    data = JSON.parse(response.body)
+    assert_equal ["reservation_id", "rating", "comment"], data.keys
+  end
+  
+  test "update" do
+    review = Review.first
+    patch "/reviews/#{review.id}.json", params: { comment: "comment" }
+    assert_response 200
+
+    data = JSON.parse(response.body)
+    assert_equal "comment", data["comment"]
+  end
+
+  test "destroy" do
+    assert_difference "Review.count", -1 do
+      delete "/reviews/#{Review.first.id}.json"
+      assert_response 200
+    end
+  end
+
+  
 end
