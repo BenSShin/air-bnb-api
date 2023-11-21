46d4033c10c4e9770592a50c6f30baac4efddbe9
bshin27 <125755440+bshin27@users.noreply.github.com>
2023-11-21 12:23:04 -0600

    added create action

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -8,4 +8,22 @@ class ReservationsController < ApplicationController
     @reservation = Reservation.find_by(id: params[:id])
     render :show
   end
+
+  def create
+    @reservation = Reservation.new(
+      user_id: params[:user_id],
+      room_id: params[:room_id],
+      start_date: Date.parse(params[:start_date], "%Y/%m/%d"),
+      end_date: Date.parse(params[:end_date], "%Y/%m/%d"),
+      price: params[:price],
+      total: params[:total],
+    )
+
+    if @reservation.valid?
+      @reservation.save
+      render :show
+    else
+      render json: { errors: @reservation.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
 end
